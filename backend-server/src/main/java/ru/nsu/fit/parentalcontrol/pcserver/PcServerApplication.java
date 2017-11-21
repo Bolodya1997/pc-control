9079e2e426f8d6b1a40a188e13e0b14fcf239763
Bolodya <Vovan1_2@mail.ru>
2017-11-22 00:33:21 +0700

    feature(backend-server): web-server initial commit
    Closes #1431

---
+++ backend-server/src/main/java/ru/nsu/fit/parentalcontrol/pcserver/PcServerApplication.java
@@ -0,0 +1,12 @@
+package ru.nsu.fit.parentalcontrol.pcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcServerApplication.class, args);
+	}
+}
